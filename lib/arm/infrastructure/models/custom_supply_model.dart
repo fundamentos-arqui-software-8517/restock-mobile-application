f8f5842a0ebc794b0e5f86e662a3e4d56d511d66
Far14z <faridce14@gmail.com>
2026-05-26 15:18:52 -0500

    feat(custom-supply): add custom supply remote service.

---
+++ lib/arm/infrastructure/models/custom_supply_model.dart
@@ -0,0 +1,73 @@
+import 'package:restock/arm/domain/entities/custom_supply.dart';
+
+class CustomSupplyResponseModel extends CustomSupply {
+  
+  CustomSupplyResponseModel({
+    required String accountId,
+    required String name,
+    required String category,
+    required double unitPrice,
+    required double unitOfMeasure,
+    required String imageUrl,
+    required String description,
+    required double minStock,
+    required double maxStock,
+    required String createdAt,
+  }) : super(
+         accountId: accountId,
+         name: name,
+         category: category,
+         unitPrice: unitPrice,
+         unitOfMeasure: unitOfMeasure,
+         imageUrl: imageUrl,
+         description: description,
+         minStock: minStock,
+         maxStock: maxStock,
+         createdAt: createdAt,
+       );
+
+  factory CustomSupplyResponseModel.fromJson(Map<String, dynamic> json) {
+    return CustomSupplyResponseModel(
+      accountId: json['accountId'],
+      name: json['name'],
+      category: json['category'],
+      unitPrice: json['unitPrice'].toDouble(),
+      unitOfMeasure: json['unitOfMeasure'].toDouble(),
+      imageUrl: json['imageUrl'],
+      description: json['description'],
+      minStock: json['minStock'].toDouble(),
+      maxStock: json['maxStock'].toDouble(),
+      createdAt: json['createdAt'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'accountId': accountId,
+      'name': name,
+      'category': category,
+      'unitPrice': unitPrice,
+      'unitOfMeasure': unitOfMeasure,
+      'imageUrl': imageUrl,
+      'description': description,
+      'minStock': minStock,
+      'maxStock': maxStock,
+      'createdAt': createdAt,
+    };
+  }
+
+  CustomSupply toDomain() {
+    return CustomSupply(
+      accountId: accountId,
+      name: name,
+      category: category,
+      unitPrice: unitPrice,
+      unitOfMeasure: unitOfMeasure,
+      imageUrl: imageUrl,
+      description: description,
+      minStock: minStock,
+      maxStock: maxStock,
+      createdAt: createdAt,
+    );
+  }
+}
