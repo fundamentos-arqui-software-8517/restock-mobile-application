708249951bf27a42b26ff4c4b138dfcf787fb241
Yaku Guzman <yakumgc12@gmail.com>
2026-06-07 16:31:31 -0500

    chore(devices): add device endpoints to api constants

--- lib/shared/infrastructure/constants/api_constants.dart
+++ lib/shared/infrastructure/constants/api_constants.dart
@@ -37,4 +37,19 @@ class ApiConstants {
   static final String supplies = 'supplies';
 
   static final String suppliesCategories = 'supplies/categories';
+
+  // Devices endpoints
+  static final String devices = 'devices';
+  static String deviceById = 'devices/{deviceId}';
+  static String deviceSpecifications = 'devices/{deviceId}/specifications';
+  static String deviceConfigBranch = 'devices/{deviceId}/configuration/branch';
+  static String deviceConfigBatch = 'devices/{deviceId}/configuration/batch';
+  static String deviceConfigThreshold = 'devices/{deviceId}/configuration/threshold';
+  static String deviceConfigMeasurement = 'devices/{deviceId}/configuration/measurement';
+  static String deviceStatus = 'devices/{deviceId}/status';
+  static String deviceWithdrawnStock = 'devices/{deviceId}/withdrawn-stock';
+
+  // Device thresholds endpoints
+  static final String deviceThresholds = 'device-thresholds';
+  static String deviceThresholdById = 'device-thresholds/{thresholdId}';
 }
